3c4f06a76a846a14bd38c31e541a326359d900d9
Kirill Galushko <yoursgk@mail.ru>
2024-11-09 21:52:41 +0600

    Add new build script

--- index.ts
+++
@@ -1,3 +0,0 @@
-// @ts-ignore import.meta.globEager is provided by Vite
-const icons = import.meta.globEager('./**/*.svg')
-export default icons
--- vite.config.ts
+++ vite.config.ts
@@ -1,67 +1,48 @@
 import { defineConfig } from 'vite'
-import svgLoader from 'vite-svg-loader'
-import { resolve, relative } from 'path'
-import fs from 'fs'
-
-function getSvgFilesRecursively(dir: string): string[] {
-  const entries = fs.readdirSync(dir, { withFileTypes: true })
-  const svgFiles = entries.flatMap((entry) => {
-    const fullPath = resolve(dir, entry.name)
-    return entry.isDirectory() ? getSvgFilesRecursively(fullPath)
-                               : entry.name.endsWith('.svg') ? [fullPath] : []
-  })
-  return svgFiles
-}
-
-const iconsDir = resolve(__dirname, 'public/svg')
-const svgFiles = getSvgFilesRecursively(iconsDir)
-
-const svgoConfig = {
-  plugins: [
-    {
-      name: 'removeAttrs' as const,
-      params: {
-        attrs: '(style)'
-      }
-    },
-    {
-      name: 'addAttributesToSVGElement' as const,
-      params: {
-        attributes: [
-          { style: 'width: 1em; min-width: 1em; height: 1em; min-height: 1em;' }
-        ]
-      }
-    }
-  ]
-}
+import vue from '@vitejs/plugin-vue'
+import { resolve, relative, dirname, basename } from 'path'
+import dts  from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    svgLoader({
-      svgoConfig,
+  plugins: [vue(),
+    dts({
+      rollupTypes: true
     }),
   ],
   build: {
     rollupOptions: {
       external: ['vue'],
-      input: Object.fromEntries(
-        svgFiles.map((file) => [
-          relative(iconsDir, file).replace('.svg', ''),
-          file,
-        ])
-      ),
       output: {
         globals: {
           vue: 'Vue',
         },
-        entryFileNames: ({ name }) => `${name}.js`,
-        format: 'es',
+        format: 'esm',
         dir: 'dist',
+        manualChunks(id) {
+          if (id.includes('.vue')) {
+            const folder = relative(__dirname, dirname(id)).replace(/\\/g, '/').replace('build/', ''); // Используем правильный формат пути
+            if (!folder) {
+              return null
+            }
+            const fileName = basename(id, '.vue');
+            return `${folder}/${fileName}`;
+          }
+        },
+        chunkFileNames: (chunk) => {
+          if (!chunk.facadeModuleId) {
+            return '[name].js';
+          }
+          const folderName = dirname(chunk.facadeModuleId);
+          const chunkName = basename(chunk.facadeModuleId, '.vue');
+          return `${folderName}/${chunkName}.js`;
+        },
+
       },
     },
     lib: {
-      entry: resolve(__dirname, 'index.ts'),
+      entry: resolve(__dirname, 'build/index.ts'),
       formats: ['es'],
+      fileName: (format) => `gui-icons.${format}.js`,
     },
   },
 })
